ca0c58aeb8d2ff193e0d5925d82854e006ec30f8
unknown <barrettrules@gmail.com>
2021-09-14 22:49:57 -0500

    pizza time

--- deploy-commands.js
+++ deploy-commands.js
@@ -5,7 +5,15 @@ const { clientId, token } = require('./config.json');
 
 const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-    new SlashCommandBuilder().setName('pizzatime').setDescription('Pizza Time'),
+	new SlashCommandBuilder().setName('pizzatime').setDescription('Pizza Time'),
+	new SlashCommandBuilder()
+		.setName('pizzaparker')
+		.setDescription('Pizza Time!')
+		.addChannelOption(
+			(option) => option
+				.setName('channel-to-join')
+				.setDescription('Overrides current user\'s channel for pizza time if user is in a voice channel.')
+		)
 ]
 	.map(command => command.toJSON());
 
